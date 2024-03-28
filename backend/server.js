a1ec621baf3dfba230583bec794c14bf47e03a92
Ajay v <vajay4834@gmail.com>
2024-03-28 13:03:44 +0530

    models created

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./DB/connectDB.js";
+
+dotenv.config();
+connectDB();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server started at http://localhost:${PORT}`)
+);
